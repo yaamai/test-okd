4f92371ba9eab3da2a467445c4750effd4c79d20
yaamai <ya_amai@yahoo.co.jp>
2022-04-06 04:01:35 +0900

    initial commit

---
+++ pxe/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+cleanup() {
+  ip addr del dev $interface $server_ip/$server_prefix_len || true
+}
+
+main() {
+  local interface=${PXE_INTF:-br0}
+  local server_ip=${PXE_IP:-10.101.101.1}
+  local server_prefix_len=${PXE_IP_LEN:-24}
+  local server_url=${PXE_URL:-http://$server_ip:8000}
+  local subnet=${PXE_SUBNET:-10.101.101.100,10.101.101.150}
+
+  trap cleanup EXIT
+
+  ip addr add dev $interface $server_ip/$server_prefix_len || true
+  ip link set dev $interface up || true
+
+  sed 's$__SERVER__$'$server_url'$g' -i $PWD/tftpboot/pxelinux.cfg/default
+
+  dnsmasq \
+    -d \
+    -R \
+    -z \
+    -i $interface \
+    -F $subnet \
+    --enable-tftp \
+    --tftp-root=$PWD/tftpboot \
+    --dhcp-boot=/pxelinux.0 &
+
+  cd $PWD/http && python3 -m http.server &
+
+  wait -n
+}
+
+main
